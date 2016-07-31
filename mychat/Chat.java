892a63747c7a0dd97f2cb66d65c355767c312884
wongpeeng <635711867@qq.com>
2016-07-31 21:10:53 +0800

    chat

--- mychat/Chat.java
+++ mychat/Chat.java
@@ -8,12 +8,12 @@ import java.awt.*;
 public class Chat{
     private JFrame frame=new JFrame("wongpeeng's first chat program");
     private JLabel contentLabel=new JLabel("chat content:");
-    private JLabel toSendLabel=new JLabel("what to say:");
-    private JButton setButton=new JButton("set");
+    private JLabel sendLabel=new JLabel("what to say:");
     private JButton saveButton=new JButton("save");
     private JButton sendButton=new JButton("send");
-    private JTextArea contentArea=new JTextArea(20,400);
-    private JTextArea toSendArea=new JTextArea(20,400);
+    private JButton disconButton=new JButton("disconnect");
+    private JTextArea contentArea=new JTextArea(20,20);//row,col
+    private JTextArea sendArea=new JTextArea(20,20);
     private JDialog dialog=new JDialog(frame,"Configuration");
     public Chat(){
        // System.out.println("hello!");
@@ -33,7 +33,7 @@ public class Chat{
         frame.setVisible(true);
         frame.setResizable(true);
 
-        dialog.setBounds(200,200,200,250);
+        dialog.setBounds(200,200,300,250);
         dialog.setLayout(new GridLayout(4,2,10,10));
         Container dpanel=dialog.getContentPane();
         JLabel ipLabel=new JLabel("destination IP");
@@ -53,6 +53,26 @@ public class Chat{
         dpanel.add(new JLabel("     "));
         dpanel.add(sureButton);
         dialog.setVisible(true);//must set visible after adding,not before
+
+
+        JPanel contentPanel=new JPanel();
+        JPanel sendPanel=new JPanel();
+        JPanel buttonPanel=new JPanel();
+        contentPanel.setLayout(new GridLayout(2,1,10,15));
+        contentPanel.add(contentLabel);
+        contentPanel.add(contentArea);
+        sendPanel.setLayout(new GridLayout(2,1,10,15));
+        sendPanel.add(sendLabel);
+        sendPanel.add(sendArea);
+        buttonPanel.setLayout(new GridLayout(1,3,10,10));
+        buttonPanel.add(disconButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(sendButton);
+
+        fpanel.add(contentPanel);
+        fpanel.add(sendPanel);
+        fpanel.add(buttonPanel);
+        frame.setVisible(true);//set true after adding not before
 
     }
 }
